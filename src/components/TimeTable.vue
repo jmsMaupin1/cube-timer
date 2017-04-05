4691d7fb93b260eccf286f39b796ac7b29a544a6
James Maupin <jmsMaupin1@yahoo.com>
2017-04-04 22:04:41 -0400

    Implement time and average tracking

--- src/components/TimeTable.vue
+++ src/components/TimeTable.vue
@@ -1,11 +1,20 @@
 <template>
   <el-card class='time-table'>
-    <el-table border :data='tableData' style='width: 100%'>
-      <el-table-column type='index'>
-      </el-table-column>
-      <el-table-column prop='time' label='Time'>
-      </el-table-column>
+    <el-table border 
+      :data='tableData'
+      max-height=520 
+      empty-text=' '>
+      <el-table-column type='index' width=80 />
+      <el-table-column prop='time' label='Time' />
     </el-table>
+
+    <div class='ao5'>
+      Average Of 5: <span class='average'>{{averageOf5}}</span>
+    </div>
+
+    <div class='ao12'>
+      Average Of 12: <span class='average'>{{averageOf12}}</span>
+    </div>
   </el-card>
 </template>
 
@@ -18,13 +27,38 @@ export default {
 
   data () {
     return {
-      tableData: [{}]
+      tableData: []
     }
   },
   mounted: function () {
     EventBus.$on('store', payload => {
       this.tableData.push(payload)
     })
+  },
+  computed: {
+    averageOf5: function () {
+      let tableLength = this.tableData.length
+      if (tableLength > 4) {
+        return (this.tableData.slice(tableLength - 5)
+          .reduce((total, next) => {
+            return total + parseFloat(next.time)
+          }, 0) / 5).toFixed(3)
+      } else {
+        return 'N/A'
+      }
+    },
+
+    averageOf12: function () {
+      let tableLength = this.tableData.length
+      if (tableLength > 11) {
+        return (this.tableData.slice(tableLength - 12)
+          .reduce((total, next) => {
+            return total + parseFloat(next.time)
+          }, 0) / 12).toFixed(3)
+      } else {
+        return 'N/A'
+      }
+    }
   }
 }
 </script>
@@ -33,7 +67,15 @@ export default {
   .time-table {
     float: left;
     margin-top: 1%;
-    width: 15%;
+    width: 17%;
     height: 80vh;
+  }
+
+  .ao5, .ao12 {
+    padding-top: 20px;
+  }
+
+  .average {
+    float: right;
   }
 </style>
